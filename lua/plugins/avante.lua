c766efe37ab6a312553af0f97ffa62080f2e8140
Vo Nhat Trieu <vntrieu25879.com>
2025-08-22 10:22:41 +0700

    Update avante.lua

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,36 +4,41 @@ return {
     event = "VeryLazy",
     version = false,
     opts = {
-      debug = true,
-      provider = "copilot",
-      auto_suggestions_provider = nil,
+      providers = {
+        debug = true,
+        provider = "copilot",
+        auto_suggestions_provider = nil,
 
-      -- OpenAI Config
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        -- OpenAI Config
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+          extra_request_body = {
+            temperature = 0,
+            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          },
+        },
+        -- Ollama Config
+        ollama = {
+          model = "qwen3:1.7b",
+        },
       },
-      -- Ollama Config
-      ollama = {
-        model = "qwen3:1.7b",
+      build = "make",
+      dependencies = {
+        "zbirenbaum/copilot.lua", -- Required for copilot provider
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "hrsh7th/nvim-cmp",
+        "nvim-tree/nvim-web-devicons",
+        -- Optional:
+        "echasnovski/mini.pick",
+        "nvim-telescope/telescope.nvim",
+        "ibhagwan/fzf-lua",
       },
-    },
-    build = "make",
-    dependencies = {
-      "zbirenbaum/copilot.lua", -- Required for copilot provider
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-tree/nvim-web-devicons",
-      -- Optional:
-      "echasnovski/mini.pick",
-      -- "nvim-telescope/telescope.nvim",
-      "ibhagwan/fzf-lua",
     },
   },
 }
